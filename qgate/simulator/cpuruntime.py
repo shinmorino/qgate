512abed390c5a60b20e9a5291f1791773807e6d9
Shinya Morino <shin.morino@gmail.com>
2019-01-01 00:11:52 +0900

    Fix: interfaces are not correctly updated.

--- qgate/simulator/cpuruntime.py
+++ qgate/simulator/cpuruntime.py
@@ -3,9 +3,9 @@ import numpy as np
 from .native_qubit_processor import NativeQubitProcessor
 from .native_qubit_states import NativeQubitStates
 
-def create_qubit_states(processor) :
-    ptr = cpuext.qubit_states_new(processor.dtype)
-    return NativeQubitStates(ptr, proc)
+def create_qubit_states(dtype, processor) :
+    ptr = cpuext.qubit_states_new(dtype)
+    return NativeQubitStates(ptr, processor)
 
 def create_qubit_processor(dtype) :
     return NativeQubitProcessor(dtype, cpuext.qubit_processor_new(dtype))
