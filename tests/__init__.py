ff1c46606f321ecfa84fdf20ccacf94e730450f1
Shinya Morino <shin.morino@gmail.com>
2019-01-05 17:46:52 +0900

    Updating test_calc_prob.

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,4 +6,5 @@ from .test_reset import *
 from .test_if import *
 from .test_multidevice import *
 from .test_big_circuits import *
+from .test_calc_prob import *
 from .test_cuda_enabled import *
--- tests/test_calc_prob.py
+++ tests/test_calc_prob.py
@@ -14,8 +14,8 @@ class TestCalcProbBase(SimulatorTestBase) :
             raise unittest.SkipTest()
         super(TestCalcProbBase, cls).setUpClass()
 
-    def run_sim(self) :
-        sim = self._run_sim()
+    def run_sim(self, isolate_circuits = False) :
+        sim = self._run_sim(isolate_circuits)
         return sim.get_qubits()
 
     def assertAlmostEqual(self, expected, actual) :
@@ -72,6 +72,24 @@ class TestCalcProbBase(SimulatorTestBase) :
                     self.assertAlmostEqual(0.5, qubits.calc_probability(qreg[obs_idx]))
                 else :
                     self.assertAlmostEqual(1., qubits.calc_probability(qreg[obs_idx]))
+
+    def test_calc_prob_cx_isolated(self) :
+        new_program()
+        qreg = allocate_qreg(10)
+        creg = allocate_creg(10)
+        op(x(qreg[0]))
+        op(x(qreg[2]))
+        op(x(qreg[4]))
+        op(x(qreg[6]))
+        op(x(qreg[8]))
+        op(cx(qreg[0], qreg[1]))
+        op(cx(qreg[2], qreg[3]))
+        op(cx(qreg[4], qreg[5]))
+        op(cx(qreg[6], qreg[7]))
+        op(cx(qreg[8], qreg[9]))
+        qubits = self.run_sim(True)
+        for obs_idx in range(0, 10) :
+            self.assertAlmostEqual(0., qubits.calc_probability(qreg[obs_idx]))
                     
 import sys
 this = sys.modules[__name__]
