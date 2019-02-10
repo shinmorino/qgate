bf07aa4c2699a83acc1d27218dc4773a848c7851
Shinya Morino <shin.morino@gmail.com>
2019-02-10 22:18:14 +0900

    Fix: index out of range on setting bit pos.

--- qgate/simulator/src/BitPermTable.cpp
+++ qgate/simulator/src/BitPermTable.cpp
@@ -14,22 +14,15 @@ IdList qgate::createBitShiftMap(const int bitPos, int size) {
 
 IdList qgate::createBitShiftMap(const IdList &bitPosList, int size) {
     
-#if 0
-    /* FIXME: std::vector<>::resize() did not work on g++. */
     qgate::IdList bitShiftMap(size);
-#else
-    qgate::IdList bitShiftMap;
-    for (int idx = 0; idx < size; ++idx)
-        bitShiftMap.push_back(0);
-#endif
-    assert(idxToQstateIdx.size() == size);
     if (size != 0) {
         IdList sorted = bitPosList;
         std::sort(sorted.begin(), sorted.end());
         for (int idx = 0; idx < (int)sorted.size(); ++idx) {
             int inBitPos = sorted[idx] - idx;
-            assert(inBitPos < size);
-            bitShiftMap[inBitPos] += 1;
+            assert(inBitPos < size + bitPosList.size());
+            if (inBitPos < size)
+                bitShiftMap[inBitPos] += 1;
         }
         for (int idx = 0; idx < size - 1; ++idx)
             bitShiftMap[idx + 1] += bitShiftMap[idx];
