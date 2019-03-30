b5ccbccd7240a8d0280fac3c63b6c7cfb69cb7c9
Shinya Morino <shin.morino@gmail.com>
2019-03-30 18:23:42 +0900

    Adding simple_hadamard.py.

---
+++ examples/simple_hadamard.py
@@ -0,0 +1,24 @@
+import qgate
+from qgate.script import *
+
+# creating a quantum register that represent a qubit like OpenQASM.
+qreg = new_qreg()
+
+# creating a quantum circuit with H gate.
+circuit = [H(qreg)]
+
+# creating reference as a placeholder to store measured result.
+ref = new_reference()
+
+# measure qreg
+circuit.append(measure(ref, qreg))
+
+# creating simulator instance
+sim = qgate.simulator.cpu()
+
+# running the prepared circuit on sim.
+sim.run(circuit)
+
+# showing a measured value.
+value = sim.values.get(ref)
+print('Measured value: ' + str(value))
