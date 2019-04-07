aecb230cb460e89263422979f76222dbfa25966f
Shinya Morino <shin.morino@gmail.com>
2019-04-07 11:03:20 +0900

    Fixed issues for inconsistency between self.qregset and self.qregsetlist. Refactored by using frozenset(). seperate_qreg() is added to support Decohere.

--- qgate/model/qreg_aggregator.py
+++ qgate/model/qreg_aggregator.py
@@ -1,6 +1,3 @@
-from . import model
-from .pseudo_operator import ClauseBegin, ClauseEnd
-
 
 class QregAggregator :
     
@@ -15,6 +12,7 @@ class QregAggregator :
         for qregset in self.qregsetlist :
             if qreg in qregset :
                 return qregset
+        # for debug
         print('qreg {}'.format(qreg.id))
         for qregset in self.qregsetlist :
             ids = ['{}'.format(qreg.id) for qreg in qregset]
@@ -24,24 +22,50 @@ class QregAggregator :
     def add_qreg(self, qreg) :
         if not qreg in self.qregset :
             self.qregset.add(qreg)
-            self.qregsetlist.append({qreg})
+            self.qregsetlist.append(frozenset([qreg]))
             return True
         return False
-            
-    def aggregate(self, qreglist) :
-        qridset = set()
-        for qreg in qreglist :
-            qregset = self._find_qregset(qreg)
-            qridset.add(id(qregset))
-        if len(qridset) == 1 :
-            return None
-        
-        merged = set()
-        for qreg in qreglist :
-            qregset = self._find_qregset(qreg)
-            self.qregsetlist.remove(qregset)
-            merged |= qregset
-        self.qregsetlist.append(merged)
 
+    def aggregate(self, qreglist) :
+        # add qreg if not added yet.
+        for qreg in qreglist :
+            if not qreg in self.qregset :
+                self.add_qreg(qreg)
+        # collect qregsets that contain qregs in the given qreglist
+        qsset = set()
+        for qreg in qreglist :
+            qregset = self._find_qregset(qreg)
+            qsset.add(qregset)
+        # already aggregated.
+        if len(qsset) == 1 :
+            return None
+
+        # merge qregset, and update qregsetlist.
+        merged = set()
+        for qs in qsset :
+            self.qregsetlist.remove(qs)
+            merged |= qs
+        self.qregsetlist.append(frozenset(merged))
+        
         return merged
+
+    def separate_qreg(self, qreg) :
+        # get qregset that contains the given qreg.
+        qregset = self._find_qregset(qreg)
+        # remove qregset from qregsetlist.
+        self.qregsetlist.remove(qregset)
+        # remove qreg from qregset.
+        qregset = set(qregset)
+        qregset.remove(qreg)
+        # append 2 seperated qregsets.
+        self.qregsetlist.append(frozenset(qregset))
+        self.qregsetlist.append(frozenset([qreg]))
+
+    # for debug
+    def validate(self) :
+        import numpy as np
+
+        n_qregs_0 = np.sum([len(qregset) for qregset in self.qregsetlist])
+        n_qregs_1 = len(self.qregset)
+        assert n_qregs_0 == n_qregs_1
 
