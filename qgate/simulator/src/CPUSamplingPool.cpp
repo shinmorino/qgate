49217fe39fc2627252c5ead5d26162a36a787ba3
Shinya Morino <shin.morino@gmail.com>
2019-07-01 20:09:06 +0900

    Parallelizing cumulative sum and sumpling in CPUSampingPool.

--- qgate/simulator/src/CPUSamplingPool.cpp
+++ qgate/simulator/src/CPUSamplingPool.cpp
@@ -1,5 +1,5 @@
 #include "CPUSamplingPool.h"
-
+#include "Parallel.h"
 
 using namespace qgate_cpu;
 using qgate::QstateIdx;
@@ -9,16 +9,48 @@ template<class V>
 CPUSamplingPool<V>::CPUSamplingPool(V *prob, int nLanes,
                                     const qgate::IdList &emptyLanes) {
     nStates_ = qgate::Qone << nLanes;
-    V sum = V();
-    for (QstateIdx idx = 0; idx < nStates_; ++idx) {
-        sum += prob[idx];
-        prob[idx] = sum;
-    }
-    V norm = V(1) / prob[nStates_ - 1];
-    for (QstateIdx idx = 0; idx < nStates_; ++idx)
-        prob[idx] *= norm;
-    cumprob_ = prob;
 
+    qgate::Parallel parallel;
+    int nWorkers = parallel.getNWorkers(nStates_);
+    V *partialSum = new V[nWorkers]();
+    auto prefixSumFunc = [=](int threadIdx, QstateIdx spanBegin, QstateIdx spanEnd) {
+        V v = V(0.);
+        for (QstateIdx idx = spanBegin; idx < spanEnd; ++idx) {
+            v += prob[idx];
+            prob[idx] = v;
+        }
+        partialSum[threadIdx] = v;
+    };
+    parallel.distribute(0, nStates_, prefixSumFunc);
+    /* prefix sum for partial sum */
+    V v = V();
+    for (QstateIdx idx = 0; idx < nWorkers; ++idx) {
+        v += partialSum[idx];
+        partialSum[idx] = v;
+    }
+    /* add partial prefix sum and normalize */
+    V norm = V(1) / partialSum[nWorkers - 1];
+    auto applyPartialSum = [=](int threadIdx, QstateIdx spanBegin, QstateIdx spanEnd) {
+        if (threadIdx != 0) {
+            for (QstateIdx idx = spanBegin; idx < spanEnd; ++idx) {
+                prob[idx] += partialSum[threadIdx - 1];
+                prob[idx] *= norm;
+            }
+        }
+    };
+    parallel.distribute(0, nStates_, applyPartialSum);
+    /* free partialSum array. */
+    delete[] partialSum;
+
+    /* set cumprob array */
+    cumprob_ = prob;
+#if 0
+    for (QstateIdx idx = 0; idx < nStates_ - 1; ++idx) {
+        V diff = cumprob_[idx + 1] - cumprob_[idx];
+        abortIf(diff < 0.);
+        abortIf(1. < diff);
+    }
+#endif
     /* prep permutation */
     qgate::IdList transform = qgate::createBitShiftMap(emptyLanes, nLanes);
     perm_.init_idxToQstateIdx(transform);
@@ -32,11 +64,14 @@ CPUSamplingPool<V>::~CPUSamplingPool() {
 
 template<class V>
 void CPUSamplingPool<V>::sample(QstateIdx *observations, int nSamples, const double *rnum) {
-    for (int idx = 0; idx < nSamples; ++idx) {
+    auto sampleFunc = [=](QstateIdx idx) {
         V *v = std::upper_bound(cumprob_, cumprob_ + nStates_, (V)rnum[idx]);
         QstateIdx obs = v - cumprob_;
+        assert(0 <= obs);
+        assert(obs < nStates_);
         observations[idx] = perm_.permute(obs);
-    }
+    };
+    qgate::Parallel().for_each(0, nSamples, sampleFunc);
 }
 
 
